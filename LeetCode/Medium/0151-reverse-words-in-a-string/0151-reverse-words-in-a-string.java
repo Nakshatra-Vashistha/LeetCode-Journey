fe4a535af7739beabbb7547265f935848515d873
Naksh <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2025-05-28 14:25:42 +0530

    Time: 6 ms (84.59%), Space: 43.5 MB (40.5%) - LeetHub

--- LeetCode/Medium/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ LeetCode/Medium/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,27 +1,14 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.split(" ");
-        int left = 0;
-        int right = words.length - 1;
-
-        while(left <= right){
-            String temp = words[left];
-            words[left] = words[right];
-            words[right] = temp;
-            left++;
-            right--;
-        }
-
+        String[] words = s.trim().split("\\s+");
         StringBuilder result = new StringBuilder();
-        for(String word : words){
-            if(!word.isEmpty()){
-                if(result.length() > 0){
-                    result.append(" ");
-                }
-                result.append(word);
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            result.append(words[i]);
+            if (i > 0) {
+                result.append(" ");
             }
         }
         return result.toString();
-        
     }
 }
