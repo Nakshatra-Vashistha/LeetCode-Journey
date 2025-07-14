8986d67576f680f0808dbb1e7622c76d95c629db
Naksh <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2025-07-14 18:03:32 +0530

    Time: 6 ms (6.73%), Space: 58.6 MB (8.21%) - LeetHub

---
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int left = 0;
+        int right = height.length-1;
+
+        while(left < right){
+            int w = right - left;
+            int h = Math.min(height[left] , height[right]);
+            int res = w * h;
+            max = Math.max(max , res );
+
+            if(height[left] < height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return max;
+        
+    }
+}
