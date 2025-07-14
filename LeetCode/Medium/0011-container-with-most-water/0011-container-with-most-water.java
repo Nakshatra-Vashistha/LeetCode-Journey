4b498513f78e81e604839c6d9384618c1c68547c
Naksh <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2025-07-14 18:26:25 +0530

    Time: 0 ms (100%), Space: 57.1 MB (93.93%) - LeetHub

--- LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.java
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,28 +1,22 @@
 class Solution {
-    static{
-        for(int i =0;i < 20; i++){
-            new Solution().maxArea(new int[]{0,0});
+     static{
+        for(int i =0;i < 500; i++){
+            maxArea(new int[]{0,0});
         }
     }
-    public int maxArea(int[] height) {
-        int max = 0;
-        int left = 0;
-        int right = height.length-1;
+    public static int maxArea(int[] heights) {
+        int start = 0;
+        int end = heights.length - 1;
+        int result=0;
+        while(start<end){
 
-        while(left < right){
-            int w = right - left;
-            int h = Math.min(height[left] , height[right]);
-            max = Math.max(max , w * h);
+            int min = Math.min(heights[start],heights[end]);
 
-            if(height[left] < height[right]){
-                left++;
-            }
-            else{
-                right--;
-            }
+            result = Math.max(result,(end- start) * min);
+
+            while(start<end && heights[start]<=min) start++;
+            while(start<end && heights[end]<=min) end--;
         }
-        System.gc();
-        return max;
-        
+        return result;
     }
 }
