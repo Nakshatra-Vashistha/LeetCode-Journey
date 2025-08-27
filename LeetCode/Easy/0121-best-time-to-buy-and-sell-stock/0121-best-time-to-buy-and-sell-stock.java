e52372207e1c4d937d9f8381f157112ab06f0c23
Naksh <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2025-08-27 11:40:17 +0530

    Time: 2 ms (72.16%), Space: 61.6 MB (64.35%) - LeetHub

---
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minprice = prices[0];
+        int maxprice = 0;
+
+        for(int i = 1 ; i < prices.length ; i++){
+
+            maxprice = Math.max(maxprice , prices[i] - minprice);
+            minprice = Math.min(minprice , prices[i]);
+        }
+        return maxprice;
+        
+    }
+}
