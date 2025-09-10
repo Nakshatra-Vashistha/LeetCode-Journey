b2ab26009c0f43e2e211c18ab2a40aad21957555
Naksh <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2025-09-10 23:15:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,44 +10,6 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next == null){
-            return true;
-        }
-
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        
-
-        ListNode sec = reverse(slow);
-        ListNode first = head;
-
-        while(sec != null ){
-            if(first.val != sec.val){
-                return false;
-            }
-            first = first.next;
-            sec = sec.next;
-
-        }
         return true;
-
-
-
-        
     }
-    public ListNode reverse(ListNode head){
-            ListNode prev = null;
-            ListNode curr = head;
-            while(curr != null){
-                ListNode nxt = curr.next;
-                curr.next = prev;
-                prev = curr;
-                curr = nxt;
-            }
-            return prev;
-        }
 }
