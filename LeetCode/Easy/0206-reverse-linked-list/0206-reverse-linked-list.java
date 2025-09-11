b30ca40e8a509bc22f4902faa18a9b0e8ce32a24
Naksh <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2025-09-11 21:13:59 +0530

    Time: 0 ms (100%), Space: 42.5 MB (52.35%) - LeetHub

--- LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.java
+++ LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -12,20 +12,18 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         if(head == null || head.next == null){
             return head;
+
         }
-
-        ListNode prev = null;
-        ListNode curr = head;
-
+        ListNode prev = head;
+        ListNode curr = head.next;
         while(curr != null){
             ListNode nxt = curr.next;
             curr.next = prev;
             prev = curr;
             curr = nxt;
-            
-
         }
-        
+
+        head.next = null;
         head = prev;
 
         return head;
