f36d3870ce4616d27de9acf8d095c9d373ca16d2
Naksh <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2025-09-28 12:58:02 +0530

    Time: 1 ms (97.44%), Space: 46.3 MB (8.07%) - LeetHub

---
+++ LeetCode/Easy/0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(n == 0) return true;
+
+        int len = flowerbed.length;
+
+        for(int i = 0 ; i < len && n > 0  ; i++){
+            boolean left = i == 0 || flowerbed[i-1] == 0;
+            boolean right = i == len - 1 || flowerbed[i+1] == 0;
+            if(left && right && flowerbed[i] == 0){
+                flowerbed[i] = 1;
+                n--;
+            }
+            
+        }
+
+        return n == 0 ? true : false;
+        
+    }
+}
