cfac86fe09a4fc0576d6cdf3b444a680697fca9e
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2025-12-02 10:01:23 +0530

    Time: 0 ms (100%), Space: 40.4 MB (83.81%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,12 +3,12 @@ class Solution {
         if(n == 0 || n == 1){
             return 1;
         }
-        int prev = 1 , curr = 1;
+        int p = 1 , c = 1;
         for(int i = 2 ; i <= n ; i++){
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
+            int temp = c;
+            c = p + c;
+            p = temp;
         }
-        return curr;
+        return c;
     }
 }
