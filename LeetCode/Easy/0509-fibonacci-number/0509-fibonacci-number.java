7f91d33a8c3114c0a1c3384f4f3ce85cee0cdb80
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2025-12-02 09:14:12 +0530

    Time: 0 ms (100%), Space: 42.1 MB (11.62%) - LeetHub

---
+++ LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if(n <= 1) return n ;
+        int[] fibs = new int[n+1];
+
+        fibs[0] = 0;
+        fibs[1] = 1;
+
+        for(int i = 2 ; i <= n ; i++){
+            fibs[i] = fibs[i-1]+ fibs[i-2];
+        }
+        return fibs[n];
+    }
+}
