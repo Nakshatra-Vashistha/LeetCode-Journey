c0beda6b6c8d43ac5fbb9f50cbfa9ce3aec1e11c
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2025-12-23 19:10:28 +0530

    Time: 33 ms (8.32%), Space: 109.4 MB (5.06%) - LeetHub

---
+++ LeetCode/Medium/1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[1001][1001];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        return lcs(text1, text2 , n , m , dp);
+    }
+
+    public int lcs(String text1 , String text2 , int n , int m , int[][] dp){
+        if(n == 0 || m == 0){
+            return 0;
+        }
+
+        if(dp[n][m] != -1){
+            return dp[n][m];
+        }
+
+        if(text1.charAt(n-1) == text2.charAt(m-1)){
+            dp[n][m] =  1 + lcs(text1 , text2 , n-1 , m-1 , dp);
+        }
+        else{
+            dp[n][m] = Math.max(lcs(text1 , text2 , n-1 , m , dp) , lcs(text1 , text2 , n , m-1 , dp));
+
+            
+        }
+
+        return dp[n][m];
+
+    }
+}
