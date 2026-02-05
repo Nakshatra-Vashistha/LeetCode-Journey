dd1aa14d412e3f3d85a66ae32ca5435862f4b19e
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2026-02-05 14:07:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Medium/1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,41 @@
+class Solution {
+    static int dr[] = {-1,1,0,0};
+    static int dc[] = {0,0,-1,1};
+    static int rowl,coll;
+    public int numEnclaves(int[][] grid) {
+        rowl = grid.length;
+        coll = grid[0].length;
+
+        for(int i = 0 ; i < coll ; i++){
+            if(grid[i][0] == 1) dfs(i , 0 , grid);
+            if(grid[i][coll-1] == 1) dfs(i , coll-1 , grid);
+        }
+        for(int j = 0 ; j < rowl ; j++){
+            if(grid[0][j] == 1) dfs(0 , j , grid);
+            if(grid[rowl-1][j] == 1) dfs(rowl-1 , j , grid);
+        }
+        int res = 0;
+
+        for(int i = 0 ; i < rowl ; i++){
+            for(int j = 0 ; j < coll ; j++){
+                if(grid[i][j] == 1){
+                    res++;
+                }
+            }
+        }
+
+        return res;
+
+        
+    }
+
+    public static void dfs(int r , int c , int[][] grid){
+        if(r < 0 || c < 0 || r >= rowl || c >= coll || grid[r][c] == 0){
+            return;
+        }
+        grid[r][c] = 0;
+        for(int i = 0 ; i < 4 ; i++){
+            dfs(r+dr[i] , c+dc[i],grid);
+        }
+    }
+}
