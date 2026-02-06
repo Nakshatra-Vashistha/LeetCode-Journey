10bfe72ad52284860542ea23396bfdac11c412d1
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2026-02-06 11:58:53 +0530

    Time: 6 ms (23.4%), Space: 51.5 MB (87.83%) - LeetHub

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
@@ -1,36 +1,56 @@
 class Solution {
-    static int dr[] = {-1,1,0,0};
-    static int dc[] = {0,0,-1,1};
-    static int rowl;
-    static int coll;
+    static int rLen;
+    static int cLen;
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,-1,1};
+    static char[] direction = {'U','D','L','R'};
+
+    static class pair{
+        int row;
+        int col;
+        public pair(int row , int col){
+            this.row = row;
+            this.col = col;
+        }
+    }
     public int numIslands(char[][] grid) {
-        rowl = grid.length;
-        coll = grid[0].length;
+        rLen = grid.length;
+        cLen = grid[0].length;
 
-        int islands = 0;
-
-        for(int i = 0 ; i < rowl ; i++){
-            for(int j = 0 ; j < coll ; j++){
-                if(grid[i][j] =='1'){
-                    dfs(i , j , grid);
-                    islands++;
+        boolean[][] vis = new boolean[rLen][cLen];
+        int components = 0;
+        for(int i = 0 ; i < rLen ; i++){
+            for(int j = 0 ; j < cLen ; j++){
+                if(!vis[i][j] && grid[i][j] == '1'){
+                    bfs(i,j,vis,grid);
+                    components++;
                 }
             }
         }
-        return islands;
-        
+        return components;
         
     }
 
-    static void dfs(int r , int c , char[][] grid){
-        if(r < 0 || c < 0 || r >= rowl || c >= coll || grid[r][c] == '0'){
-            return ;
-        }
+    public void bfs(int r , int c , boolean[][] vis , char[][] grid){
+        Queue<pair> q = new LinkedList<>();
+        vis[r][c] = true;
 
-        grid[r][c] = '0';
+        q.offer(new pair(r,c));
 
-        for(int i = 0 ; i < 4 ; i++){
-            dfs(r+dr[i] , c+dc[i] , grid);
+        while(!q.isEmpty()){
+            pair curr = q.poll();
+            int R = curr.row;
+            int C = curr.col;
+
+            for(int i = 0 ; i < 4 ; i++){
+                int nRow = R+dr[i];
+                int nCol = C + dc[i];
+
+                if(nRow >= 0 && nCol >= 0 && nRow < rLen && nCol < cLen && !vis[nRow][nCol] && grid[nRow][nCol] == '1'){
+                    vis[nRow][nCol] = true;
+                    q.offer(new pair(nRow,nCol));
+                }
+            }
         }
     }
 }
