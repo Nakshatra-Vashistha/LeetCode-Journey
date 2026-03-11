7d3c43dec7376839aa1061ceec697c3f07bd0795
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2026-03-11 10:29:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Hard/0968-binary-tree-cameras/0968-binary-tree-cameras.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static int cam;
+    public int minCameraCover(TreeNode root) {
+        cam = 0;
+
+        if(helper(root) == 0){
+            cam++;
+        }
+
+
+        return cam;
+        
+    }
+
+    public static int helper(TreeNode node){
+        if(node == null){
+            return 2;
+        }
+
+        int left = helper(node.left);
+        int right = helper(node.right);
+
+        if(left == 0 || right == 0){
+            cam++;
+            return 1;
+        }
+        if(left == 1 || right == 1){
+            return 2;
+        }
+
+        return 0;
+    }
+}
