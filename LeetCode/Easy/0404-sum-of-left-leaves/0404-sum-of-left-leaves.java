16de0c78d47e8514b41a30fa0fb1cdd5e3b02d3d
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2026-05-04 13:48:39 +0530

    Time: 0 ms (100%), Space: 43.4 MB (12.35%) - LeetHub

---
+++ LeetCode/Easy/0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int count;
+    public int sumOfLeftLeaves(TreeNode root) {
+        count = 0;
+        leftSum(root);
+        return count;
+    }
+
+    public void leftSum(TreeNode root){
+        if(root == null){
+            return;
+        }
+
+        if(root.left != null && root.left.left == null && root.left.right == null){
+            count += root.left.val;
+        }
+
+        leftSum(root.left);
+        leftSum(root.right);
+
+        
+    }
+}
