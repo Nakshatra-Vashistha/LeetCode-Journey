d7382e3785cc913c40071c4f2e4c6b9ad46e83c3
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2026-05-05 11:54:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ LeetCode/Medium/0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,25 +10,26 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-
-        if(head == null || head.next == null){
-            return head;
-        }
+        ListNode evenHead = head.next;
 
         ListNode odd = head;
         ListNode even = head.next;
-        ListNode evenkahead = even;
 
-        while(even != null && even.next != null){
-            odd.next = even.next;
-            odd = odd.next;
+        while(odd.next != null && even.next != null){
+            odd.next = odd.next.next;
+            odd = even.next;
 
-            even.next = odd.next;
-            even = even.next;
+            even.next = even.next.next;
+            even = odd.next;
+
         }
-        odd.next = evenkahead;
+
+        odd.next = evenHead;
         return head;
 
-        
+
+
+
+
     }
 }
