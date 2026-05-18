db16d3eb073fd25510aa4e668b922bc44c73a502
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2026-05-18 12:45:46 +0530

    Time: 1342 ms (29.53%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/1158-market-analysis-i/1158-market-analysis-i.sql
+++ LeetCode/Medium/1158-market-analysis-i/1158-market-analysis-i.sql
@@ -2,7 +2,6 @@
 SELECT u.user_id as buyer_id  , u.join_date  , COUNT(o.order_id) as orders_in_2019 
 FROM Users u 
 LEFT JOIN Orders o  ON u.user_id = o.buyer_id AND
-    o.order_date >= '2019-01-01' AND
-    o.order_date <= '2019-12-31'
+    YEAR(o.order_date) = '2019'
 GROUP BY u.user_id , u.join_date
 ORDER BY u.user_id , u.join_date;
