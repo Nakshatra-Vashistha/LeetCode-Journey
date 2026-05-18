d84c7f5bca9bcc382f330c702d9e331530ca8609
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2026-05-18 12:42:36 +0530

    Time: 1488 ms (13.27%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/Medium/1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT u.user_id as buyer_id  , u.join_date  , COUNT(o.order_id) as orders_in_2019 
+FROM Users u 
+LEFT JOIN Orders o  ON u.user_id = o.buyer_id AND
+    o.order_date >= '2019-01-01' AND
+    o.order_date <= '2019-12-31'
+GROUP BY u.user_id , u.join_date
+ORDER BY u.user_id , u.join_date;
