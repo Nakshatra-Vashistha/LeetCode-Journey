b9c08ff58445b667f76efb9f4ae2d894547d2dab
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2026-05-18 11:35:37 +0530

    Time: 7 ms (64.59%), Space: 46.8 MB (21.05%) - LeetHub

--- LeetCode/Medium/0494-target-sum/0494-target-sum.java
+++ LeetCode/Medium/0494-target-sum/0494-target-sum.java
@@ -1,18 +1,29 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums , target , 0 , 0);
+        int total = 0;
+        for(int num : nums){
+            total += num;
+        }
+
+
+        Integer[][] dp = new Integer[nums.length+1][2*total+1];
+        return helper(nums , target , 0 , 0 , dp , total);
     }
 
 
-    private int helper(int[] nums , int target , int idx, int sum){
+    private int helper(int[] nums , int target , int idx, int sum , Integer[][] dp , int offset){
         if(idx >= nums.length){
             return sum == target ? 1 : 0;
         }
 
-        int sub = helper(nums , target , idx+1 , sum - nums[idx]);
+        if(dp[idx][sum+offset] != null){
+            return dp[idx][sum+offset];
+        }
 
-        int add = helper(nums , target , idx+1 , sum + nums[idx]);
+        int sub = helper(nums , target , idx+1 , sum - nums[idx] , dp , offset);
 
-        return sub + add;
+        int add = helper(nums , target , idx+1 , sum + nums[idx] , dp , offset);
+
+        return dp[idx][sum + offset] =  sub + add;
     }
 }
