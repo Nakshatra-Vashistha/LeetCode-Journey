583e29e642d1ecf5c2ab87d35abb466048de66e8
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2026-05-19 14:34:49 +0530

    Time: 450 ms (50.79%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ LeetCode/Medium/0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,10 +1,30 @@
+-- CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+-- BEGIN
+--     SET N = N-1;
+--   RETURN (
+--       # Write your MySQL query statement below.
+--       SELECT distinct salary 
+--       FROM Employee
+--       ORDER BY salary DESC
+--       LIMIT 1 OFFSET N
+
+--   );
+-- END
+
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
+
     SET N = N - 1;
+
     RETURN (
-        SELECT DISTINCT salary
-        FROM Employee
-        ORDER BY salary DESC
-        LIMIT 1 OFFSET N
+        WITH cte AS (
+            SELECT DISTINCT salary
+            FROM Employee
+            ORDER BY salary DESC
+            LIMIT 1 OFFSET N
+        )
+
+        SELECT salary FROM cte
     );
+
 END
