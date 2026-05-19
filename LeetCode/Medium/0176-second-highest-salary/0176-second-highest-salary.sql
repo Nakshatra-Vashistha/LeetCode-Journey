e36f1778c5b41222e611498050637ceed4984892
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2026-05-19 14:18:17 +0530

    Time: 313 ms (15.3%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/0176-second-highest-salary/0176-second-highest-salary.sql
+++ LeetCode/Medium/0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,13 @@
 # Write your MySQL query statement below
-select (  
-    select distinct salary from (  
-    select salary , dense_rank() over (order by salary DESC) as Drank from Employee )t  where Drank = 2
-) as SecondHighestSalary ;
+-- select (  
+--     select distinct salary from (  
+--     select salary , dense_rank() over (order by salary DESC) as Drank from Employee )t  where Drank = 2
+-- ) as SecondHighestSalary ;
+
+
+with cte as (
+    select Max(salary) as SecondHighestSalary 
+    from Employee
+    where salary < (select Max(salary) from Employee)
+)
+select * from cte;
