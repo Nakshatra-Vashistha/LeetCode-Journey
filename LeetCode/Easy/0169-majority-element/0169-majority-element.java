a033ea31e8154124bca171642d8f985acfab4078
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2026-06-17 16:05:46 +0530

    Time: 16 ms (19.67%), Space: 52.8 MB (74.52%) - LeetHub

--- LeetCode/Easy/0169-majority-element/0169-majority-element.java
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.java
@@ -1,23 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate = -1;
-        int count = 0;
 
-        for(int num : nums){
-            if(num == candidate){
-                count++;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int n = nums.length;
+
+        for (int num : nums) {
+
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+            if (map.get(num) > n / 2) {
+                return num;
             }
-            else if(count == 0){
-                candidate = num;
-                count++;
-            }
-            else{
-                count--;
-            }
-            
         }
-        return candidate;
-        
-        
+
+        return -1;
     }
 }
