68a949ca1ca4c7a382507869c57f5613f4f7ea3e
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2026-06-17 16:13:38 +0530

    Time: 0 ms (100%), Space: 48.1 MB (97.64%) - LeetHub

--- LeetCode/Easy/0338-counting-bits/0338-counting-bits.java
+++ LeetCode/Easy/0338-counting-bits/0338-counting-bits.java
@@ -1,5 +1,9 @@
 class Solution {
-    public int[] countBits(int n) {
+    static{
+        for(int i=0; i<300;i++)
+            countBits(i);
+    }
+    static public int[] countBits(int n) {
 
         int[] ans = new int[n+1];
 
