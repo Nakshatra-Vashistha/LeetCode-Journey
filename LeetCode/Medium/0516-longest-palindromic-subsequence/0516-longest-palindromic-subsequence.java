3ef83d6c565fc5fe68fda2642b1576cc70fffb35
Nakshatra Vashistha <143750150+Nakshatra-Vashistha@users.noreply.github.com>
2026-08-04 19:06:00 +0530

    Time: 59 ms (27.98%), Space: 63.7 MB (56.62%) - LeetHub

--- LeetCode/Medium/0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ LeetCode/Medium/0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,19 +1,20 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        String r = new StringBuilder(s).reverse().toString();
+        StringBuilder sb = new StringBuilder(s).reverse();
+        String l = sb.toString();
 
         int n = s.length();
-        int m = n;
+        int m = l.length();
 
         int[][] dp = new int[n+1][m+1];
 
         for(int i = 1 ; i <= n ; i++){
             for(int j = 1 ; j <= m ; j++){
-                if(s.charAt(i-1) == r.charAt(j-1)){
+                if(s.charAt(i-1) == l.charAt(j-1)){
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
                 }
             }
         }
